6837004af00ff9a8122ae8bc905395773e852726
Davi Sobrinho Labre <davisobrinho82452@gmail.com>
2022-10-04 13:11:47 -0300

    Criei a primeira activity, uma tela branca vazia, binding já está configurada.

---
+++ app/src/main/java/com/example/pomos/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.pomos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pomos.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
