5172a6835fba7f36a33297df3460f249e1af2f6f
Davi Sobrinho Labre <davisobrinho82452@gmail.com>
2022-10-04 19:12:53 -0300

    Adicionados os demais botões da tela home e o nome do app, fora isso, está sendo feita a construção do layout para o dialog

--- app/src/main/java/com/example/pomos/view/MainActivity.kt
+++ app/src/main/java/com/example/pomos/view/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.pomos.view
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.Toast
+import com.example.pomos.R
 import com.example.pomos.databinding.ActivityMainBinding
+import com.example.pomos.databinding.DialogAddTarefaBinding
 
-private lateinit var binding: ActivityMainBinding
+private lateinit var binding: DialogAddTarefaBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = DialogAddTarefaBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        title
     }
+    /*fun configuraBotaoAdicionarTarefa(){
+        binding.button1.setOnClickListener(){
+            Toast.makeText(this, "Teste", Toast.LENGTH_SHORT).show()
+            ADDTarefaDialogFragment(1)
+        }
+    }*/
+
 }
